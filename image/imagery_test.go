60fa3d71be57bedbaa4c4ec896aba5b11a245199
Ken Faulkner <Ken.Faulkner@gmail.com>
2024-08-04 15:15:39 +1000

    dilate test

--- image/imagery_test.go
+++ image/imagery_test.go
@@ -78,6 +78,59 @@ func TestErode(t *testing.T) {
 	}
 }
 
+// TestDilate tests dilating of Suzuki Image
+func TestDilate(t *testing.T) {
+	testCases := []struct {
+		name   string
+		width  int
+		height int
+		radius int
+
+		imageData []int
+
+		// holes (ie pixels set to 0) in eroded image.
+		expectedResultData []int
+
+		expectErr bool
+	}{
+		{
+			name:               "success with internal hole",
+			width:              11,
+			height:             11,
+			radius:             1,
+			imageData:          []int{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 1, 1, 1, 0, 0, 1, 1, 1, 1, 0, 0, 1, 1, 1, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 1, 1, 0, 0, 0, 1, 1, 1, 1, 0, 1, 1, 0, 0, 0, 1, 1, 1, 1, 0, 0, 1, 1, 0, 0, 0, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+			expectedResultData: []int{1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0},
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			incomingImage := common.NewSuzukiImageFromData(tc.width, tc.height, tc.imageData)
+			resultImage, err := Dilate(incomingImage, tc.radius)
+
+			// expected error but didn't get it
+			if tc.expectErr && err == nil {
+				t.Errorf("expected error, got nil")
+			}
+
+			// didn't expect error but got one
+			if !tc.expectErr && err != nil {
+				t.Errorf("expected no error, got %v", err)
+			}
+
+			// expected and received error
+			if tc.expectErr && err != nil {
+				return
+			}
+
+			expectedImage := common.NewSuzukiImageFromData(tc.width, tc.height, tc.expectedResultData)
+			if !resultImage.Equals(expectedImage) {
+				t.Errorf("result image differs from expected")
+			}
+		})
+	}
+}
+
 // createSuzukiImage creates a dummy SuzukiImage for test purposes.
 func createSuzukiImage(width int, height int, holes []image.Point) *common.SuzukiImage {
 	si := createFullSuzukiImage(width, height)
